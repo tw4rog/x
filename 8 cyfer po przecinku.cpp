1d6658e7f807da706a9452cecabaeb42fccbdb2d
tw4rog <45147032+tw4rog@users.noreply.github.com>
2018-12-22 23:57:07 +0100

    Add files via upload

---
+++ 8 cyfer po przecinku.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int x;
+int main()
+{
+   cout.setf(ios::fixed);
+   cout.precision(8);
+
+    cout << "PODAJ WARTOSC W CM" << endl;
+    cin>> x;
+    cout << "cale sumy" <<x*2.56;
+    return 0;
+}
