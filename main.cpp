985df882cbf6286bdbbf7eec963d31011259c32b
tw4rog <45147032+tw4rog@users.noreply.github.com>
2018-12-22 23:59:16 +0100

    :P

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+string PIN;
+float x,y;
+string imie, nazwisko;
+int wybor, wybranko;
+
+int main()
+
+{
+
+{
+    cout << "Podaj imie" ; cin>> imie;
+
+}
+
+    {
+
+       cout << "Czesc" << endl;
+       cout << "Podaj swoj PIN: ";
+
+       cin >> PIN;
+     if(PIN!="fiona")
+    {
+       cout << "Zly PIN";
+       exit(0);
+    }
+     else
+
+       cout << "Dobry PIN"<< endl;
+       cout << "-.-.-.-.-.-.-.-.-.-.-.-" <<endl;
+       cout << "-.-.-.-.-MENU-.-.-.-.-.-"<< endl;
+       cout << "1.Kalkulator"<< endl;
+       cout << "2.FACEBOOK"<< endl;
+       cout << "Wybierz"<<endl;
+       cin  >> wybor;
+
+       switch(wybor)
+
+    {
+       case 1:
+       cout<< "Podaj 1 liczbe"<<endl;
+       cin >> x;
+       cout << "Podaj 2 liczbe"<<endl;
+       cin >> y;
+
+       cout<<endl;
+       cout<<"---------"<<endl;
+       cout<<"1.Dodawanie"<<endl;
+       cout<<"2.Odejmowanie"<<endl;
+       cout<<"3.Mnozenie"<<endl;
+       cout<<"4.Dzielenie"<<endl;
+       cout<<"Wybierz"<<endl;
+       break;
+       default: cout<<"Nie ma takiej opcji";
+    }
+       cin>> wybranko;
+
+
+       switch(wybranko)
+        {
+        case 1:
+           cout<< "SUMA = "<<x+y;
+        break;
+
+        case 2:
+           cout<<"ROZNICA = " <<x-y;         //tylko kalk
+        break;
+
+        case 3:
+           cout<<"ILOCZYN = " <<x*y;
+        break;
+
+        case 4:
+            cout <<"ILORAZ = " <<x/y;
+         break;
+        default: cout<<"Nie ma takiej opcji";
+       }
+
+       exit(0);
+
+
+    }
+
+
+
+     return 0;
+}
